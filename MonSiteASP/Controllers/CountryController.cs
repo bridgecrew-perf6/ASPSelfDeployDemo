67d31d511a8f7fdfaba729cc6339df1d7de535de
Samad Hatsijev <s.hatsijev@hotmail.com>
2022-01-06 15:52:22 +0100

    Feat; Country
    Test self deploy: Local -> github -> winser IIS

---
+++ MonSiteASP/Controllers/CountryController.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MonSiteASP.Models;
+
+namespace MonSiteASP.Controllers
+{
+    public class CountryController : Controller
+    {
+        // GET: CountryController
+        public ActionResult Index()
+        {
+            List<CountryModel> pays = new List<CountryModel>()
+            {
+                new ("Belgique", "Bruxelles"),
+                new ("France", "Paris"),
+                new ("Allemagne", "Berlin"),
+                new ("Pays Bas", "Amsterdam"),
+                new ("Russie", "Moscou"),
+                new ("Japon", "Tokyo"),
+                new ("Chine", "Pékin"),
+                new ("Espagne", "Madrid"),
+                new ("Portugal", "Barcelone"),
+                new ("Italie", "Rome"),
+                new ("Angleterre", "Londre"),
+            };
+            return View(pays);
+        }
+
+        // GET: CountryController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: CountryController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: CountryController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: CountryController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: CountryController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: CountryController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: CountryController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ MonSiteASP/Models/CountryModel.cs
@@ -0,0 +1,14 @@
+﻿namespace MonSiteASP.Models
+{
+    public class CountryModel
+    {
+        public string Name { get; set; }
+        public string Capital { get; set; }
+
+        public CountryModel(string name, string capital)
+        {
+            Name = name;
+            Capital = capital;
+        }
+    }
+}
